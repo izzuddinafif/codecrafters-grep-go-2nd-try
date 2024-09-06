663e972ff37e5e6f89e0e68508caebe60b272f0b
afif <izzuddinafif@gmail.com>
2024-09-06 15:50:56 +0700

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"unicode"
 )
 
-// Ensures gofmt doesn't remove the "bytes" import above (feel free to remove this!)
+var matches []rune
+
 var _ = bytes.ContainsAny
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
@@ -16,26 +18,56 @@ func main() {
 		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
 		os.Exit(2) // 1 means no lines were selected, >1 means error
 	}
-
 	pattern := os.Args[2]
 	line, _ := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
 
-	if ok, _ := matchLine(line, pattern); !ok {
+	if ok := matchLine(line, pattern); !ok {
+		fmt.Println("no match found")
 		os.Exit(1)
 	}
-
-	// default exit code is 0 which means success
+	fmt.Printf("match(es) found: %q", matches)
 }
 
-func matchLine(line []byte, pattern string) (bool, error) {
+func matchLine(line []byte, pattern string) bool {
+	var found bool
+	for i := 0; i < len(pattern); i++ {
+		pt := rune(pattern[i])
+		if pt == '\\' && i+1 < len(pattern) {
+			if pattern[i+1] == 'd' {
+				i++
+				found = matchFunc(line, unicode.IsDigit)
+			}
+		}
+		if searchFunc(line, pt) {
+			matches = append(matches, pt)
+			found = true
+		} else {
+			continue
+		}
+	}
+	return found
+}
 
-	var ok bool
+// type findMatch func(rune)
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+func matchFunc(line []byte, f func(rune) bool) bool {
+	var found bool
+	for _, b := range line {
+		li := rune(b)
+		if f(li) {
+			matches = append(matches, li)
+			found = true
+			continue
+		}
+		continue
+	}
+	return found
+}
 
-	// Uncomment this to pass the first stage
-	ok = bytes.ContainsAny(line, pattern)
-
-	return ok, nil
+func searchFunc(line []byte, pt rune) bool {
+	for _, b := range line {
+		li := rune(b)
+		return pt == li
+	}
+	return false
 }
