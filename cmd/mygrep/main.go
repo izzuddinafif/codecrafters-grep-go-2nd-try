d8f15a37847aac84e10e0ba0a838191c7e18a9a8
afif <izzuddinafif@gmail.com>
2024-09-07 11:35:00 +0700

    codecrafters submit [skip ci]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -33,41 +33,52 @@ func matchLine(line []byte, pattern string) bool {
 	var in []rune
 	for i := 0; i < len(pattern); i++ {
 		pt := rune(pattern[i])
+
 		if pt == '\\' && i+1 < len(pattern) {
 			if pattern[i+1] == 'd' {
 				i++
 				found = matchFunc(line, 0, func(r ...rune) bool {
 					return unicode.IsDigit(r[0])
 				})
+				continue
 			} else if pattern[i+1] == 'w' {
 				i++
 				found = matchFunc(line, 0, func(r ...rune) bool {
 					return unicode.IsDigit(r[0]) || unicode.IsLetter(r[0]) || r[0] == '_'
 				})
+				continue
 			}
-		}
-		if pt == '[' {
+		} else if pt == '[' {
 			squareBrackets = true
 			continue
 		}
 		if squareBrackets {
 			if pt == ']' {
 				squareBrackets = false
+				if len(in) > 0 {
+					if in[0] == '^' {
+						negatedChars := string(in[1:])
+						for _, b := range line {
+							li := rune(b)
+							if !bytes.ContainsRune([]byte(negatedChars), li) {
+								matches = append(matches, li)
+								found = true
+							}
+						}
+					} else {
+						found = bytes.ContainsAny(line, string(in))
+						matches = append(matches, findIndices(line, in)...)
+					}
+				}
+				in = nil
 				continue
 			} else {
 				in = append(in, pt)
 			}
-		}
-		if in != nil && !squareBrackets {
-			matchFunc(line, 0, func(r ...rune) bool {
-				return bytes.ContainsAny(line, string(r))
-			})
-		}
-		if matchFunc(line, pt, nil) {
-			found = true
 		} else {
-			continue
+			found = matchFunc(line, pt, nil)
 		}
+		continue
 	}
 	return found
 }
@@ -76,16 +87,42 @@ func matchFunc(line []byte, pt rune, f func(...rune) bool) bool {
 	var found bool
 	for _, b := range line {
 		li := rune(b)
-		if f != nil && f(li) { // check if f is not nil and evaluates to true
+		if f != nil {
+			fmt.Printf("Checking: %c, with function\n", li)
+			if f(li) { // check if f is not nil and evaluates to true
+				fmt.Println("Function returned true")
+				matches = append(matches, li)
+				found = true
+				continue
+			}
+		} else if pt == li {
+			fmt.Println("Direct match")
 			matches = append(matches, li)
 			found = true
-			continue
 		}
-		if pt == li {
-			matches = append(matches, li)
-			found = true
-		}
-		continue
 	}
 	return found
+}
+
+func findIndices(line []byte, chars []rune) []rune {
+	var result []rune
+	for _, b := range line {
+		li := rune(b)
+		for _, char := range chars {
+			if li == char {
+				result = append(result, li)
+				break
+			}
+		}
+	}
+	return result
+}
+
+func contains(chars []rune, r rune) bool {
+	for _, c := range chars {
+		if r == c {
+			return true
+		}
+	}
+	return false
 }
